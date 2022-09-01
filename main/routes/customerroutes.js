cb22d09d2ca9e4b9e406936ab3cd838618599132
awoods1893 <awoods1983@gmail.com>
2022-09-01 13:17:13 -0500

    Update customerroutes.js
    added more the customerroutes file

--- main/routes/customerroutes.js
+++ main/routes/customerroutes.js
@@ -12,4 +12,21 @@ exports.addCustomer = (req, res) => {
         address: Joi.string().required(),
         phone_number: Joi.number().required(),
     })
-    
+    Joi.validate(data, schema, async (err, value) => {
+        if (err){
+            res.status(422)({
+                status: 'Error',
+                message: "Invalid data input format",
+                data: data,
+            });
+        } else {
+            let customer = await models.Customer.findOne({
+                where: {email: data.email}
+            })
+            if(customer){
+                res.status(422)({
+                    status: "Error",
+                    message: "Email Address is already in use.",
+                    data: data,
+                })
+            
