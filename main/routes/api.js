654d33f2d8d924870b63ff8e0d5f9d1a56799624
awoods1893 <awoods1983@gmail.com>
2022-08-31 20:53:03 -0500

    update ordersroutes.js
    Added a route for getting an order by the customer_id and updated the api.js with the route

--- main/routes/api.js
+++ main/routes/api.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
-
 const orderRoutes = require('./ordersroutes');
 
 
 //orders
 router.post('/order/add', orderRoutes.orderAdd);
-router.get('order/id,'  )
+router.get('order/id,', orderRoutes.getOrderById);
 
 
 //customer routes
