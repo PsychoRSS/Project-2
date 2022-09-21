75b253b6804cf5dd29bd9c7934432f414fe58152
awoods1893 <awoods1983@gmail.com>
2022-09-20 19:27:45 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,31 +1,13 @@
 const Sequelize = require('sequelize');
+
+//use env files to obfuscate important DB information. .env file is in the .gitignore and not pushed to the repo
 require('dotenv').config();
-
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(
-    process.env.JAWSDB_URL,
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      port: process.env.PORT
-    }
-    );
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      port: process.env.PORT
-    }
-  );
-}
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.PORT,
+  });
 
 module.exports = sequelize;
