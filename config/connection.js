4c1f98df02db23abda5949a065bfccc67d1dd11b
awoods1893 <awoods1983@gmail.com>
2022-09-20 19:20:56 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,11 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
+    {
+      host: DB_HOST,
+      dialect: 'mysql',
+      port: process.env.PORT
+    }
     );
 } else {
   sequelize = new Sequelize(
